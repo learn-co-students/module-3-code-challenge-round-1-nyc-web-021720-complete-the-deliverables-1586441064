87bea735133329cea20c4d64343d8ac85e400bf8
osgood gunawan <ogunawan@ucsd.edu>
2020-04-09 11:10:50 -0400

    second phase

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const theatreId = 368;
 
 
-const showing= document.querySelector("ui cards showings")
+const show= document.getElementsByClassName("ui cards showings")[0]
+// const buybutton=document.getElementsByClassName("content")
+// console.log(buybutton)
 
 let url=`https://evening-plateau-54365.herokuapp.com/theatres/${theatreId}`
+
 document.addEventListener("DOMContentLoaded",function(e){
 
 
     fetchMovie();
+
+    buyticket()
 })
 
 
@@ -26,16 +31,17 @@ function fetchMovie(){
 function renderMovies(movie){
     const div= document.createElement("div")
     div.className="card"
+    div.dataset.id=movie.id
     div.innerHTML=`
     <div class="content">
       <div class="header">
          ${movie.film.title}
       </div>
       <div class="meta">
-        ${movie.film.runtime}
+        ${movie.film.runtime} minutes
       </div>
       <div class="description">
-        ${movie.capacity-movie.tickets_sold}
+        ${movie.capacity-movie.tickets_sold} remaining tickets
       </div>
       <span class="ui label">
       ${movie.showtime}
@@ -47,7 +53,23 @@ function renderMovies(movie){
   </div>
     
     `
-    showing.append(div)
- 
+ show.append(div)
 
 }
+
+
+function buyticket(){
+
+document.addEventListener("click",function(e){
+    if(e.target.textContent==="Buy Ticket"){
+        console.dir(e.target)
+        let id=e.target.parentNode.parentNode.dataset.id
+        
+        fetch()
+
+    }
+})
+
+}
+
+
