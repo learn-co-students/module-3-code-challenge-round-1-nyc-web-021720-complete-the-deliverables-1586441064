71dbc21f42d7cc1777b38085e5dfa7833c5b89a9
komald4 <komald.4@gmail.com>
2020-04-09 10:39:00 -0400

    KD

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-const theatreId = null;
+document.addEventListener('DOMContentLoaded', () => {
+
+const theatreId = 365
+const URL = `https://evening-plateau-54365.herokuapp.com/theatres/${theatreId}`
+
+
+
+//fetch list of movies from URL
+
+fetch(URL)
+.then(response => response.json())
+.then(movies => {
+    //console.log(movies)
+    movies.forEach(movie => {
+let div = renderMovie(movie)
+        main.append(div)
+    })
+})
+
+function renderMovie(movie){
+    let card = document.createElement('div')
+    card.setAttribute("class", "card")
+    card.dataset.id - movie.id
+}
+
+
+})
+
