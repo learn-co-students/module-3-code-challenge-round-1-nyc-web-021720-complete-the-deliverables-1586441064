db207c20360bbddae98fd4242dd482ff9387bb2f
Ty <tyreewilliams515@gmail.com>
2020-04-09 10:53:30 -0400

    tyree one psuedo code written

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-const theatreId = null;
+const theatreId = 375;
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://evening-plateau-54365.herokuapp.com/theatres/375')
+    .then((response) => {
+        return response.json
+    })
+    .then((data) => {
+        console.log
+    })
+})
+/* 1. When page loads, User sees as list of movie showings aka DOMContentLoaded
+  Document.addEventListener('DOMContentLoaded', () => {
+      fetch('https://evening-plateau-54365.herokuapp.com/theatres/375')
+      .then((response) => {
+          return response.json
+      })
+      .then((data) => {
+          console.log
+      })
+  })
+
+
+
+
+
+
+   2. Fetch movie showing from api and .then to get response than another .then to take the data for manipultion 
+   3. Create a button called 'Buy Ticket' and have it attached to each movie showing
+   4. Add event listner to 'Buy Ticket' button so when clicked the button decrements the remaining tickets. Also this should extend to the api
+   5. Add an if statement to prevent anyone from buying a ticket to a sold out showing.
+   5a. Should also change innerText to say 'Sold Out' instead of 'Buy Tickets'. */
