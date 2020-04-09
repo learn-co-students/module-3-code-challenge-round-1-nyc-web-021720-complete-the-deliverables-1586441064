7bf03c476f1becf5cf427dc439c314bfbb631f3b
Jared Fleming <jaredmf10@gmail.com>
2020-04-09 10:31:16 -0400

    jared first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-const theatreId = null;
+const theatreId = 360;
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // console.log('DOM fully loaded and parsed');
+    let showArea = document.getElementsByClassName("ui cards showing")
+    console.dir(showArea)
+    console.log(showArea)
+    function doesMyFetch(){
+        fetch('https://evening-plateau-54365.herokuapp.com/theatres/360')
+        .then((response) => {
+            return response.json()
+        })
+        .then((theater) => {
+            // console.log(theater.showings)
+            theater.showings.forEach(showing => {
+                let showingDiv = document.createElement('div')
+                showingDiv.dataset.showingId = showing.id
+                // showArea.appendChild(showingDiv)
+                let title = document.createElement('h3')
+                title.innerText = showing.film.title
+                showingDiv.appendChild(title)
+
+            });
+            
+        })
+    }
+    doesMyFetch()
+});
