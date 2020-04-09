c75fab031e1dc848ff541145a49fe6b14e5748be
github username <github email address>
2020-04-09 10:18:26 -0400

    fetch data from api working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-const theatreId = null;
+const theatreId = 357;
+const THEATERS_LINK = "https://evening-plateau-54365.herokuapp.com/theatres/357"
+
+document.addEventListener("DOMContentLoaded", (event => {
+
+    fetchTheaters()
+
+    function fetchTheaters() {
+        fetch(THEATERS_LINK)
+            .then(resp => resp.json())
+            .then(data => data.showings.forEach(theater => {
+                renderTheater(theater)
+            }))
+    }
+
+    function renderTheater(theater) {
+        console.log(theater)
+    }
+}))
