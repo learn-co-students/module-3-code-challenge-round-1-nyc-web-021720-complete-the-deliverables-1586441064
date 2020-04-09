52bbe163cdf427dd67d62e784ee81a638cc4c471
haihuanchen <haihuan.chen@hotmail.com>
2020-04-09 11:35:00 -0400

    fourth commit

--- src/index.js
+++ src/index.js
@@ -12,16 +12,32 @@ document.addEventListener('DOMContentLoaded', () =>{
     fetchShows(theatreId)
 
     showCards.addEventListener('click', (e) => {
-
+        if (e.target.textContent === 'Buy Ticket'){
+            let showId = e.target.parentNode.parentNode.dataset.id
+            let remainTickets = e.target.parentNode.parentNode.firstChild.nextElementSibling.firstChild.nextElementSibling.nextElementSibling.nextElementSibling
+            remainTickets --
+            fetch('https://evening-plateau-54365.herokuapp.com/tickets',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    showing_id: showId
+                })
+                
+            })
+        }
+        else if ()
+            
     })
 
 
 
 
-})
-
-function fetchShows(url){
-    fetch(url)
+    
+    function fetchShows(url){
+        fetch(url)
         .then(res => res.json())
         .then(object => {
             object.showings.forEach(showing => {
@@ -29,33 +45,35 @@ function fetchShows(url){
                 // console.log(showing)
             })
         })
-}
-
-function renderShow(showing){
-    let showCard = document.createElement('div')
-
-    let remainTickets = showing.capacity - showing['tickets_sold']
-    showCard.className = 'card'
-    showCard.innerHTML = `
+    }
+    
+    function renderShow(showing){
+        let showCard = document.createElement('div')
+        
+        let remainTickets = showing.capacity - showing['tickets_sold']
+        showCard.className = 'card'
+        showCard.dataset.id = showing.id
+        showCard.innerHTML = `
         <div class="content">
-            <div class="header">
-                ${showing.film['title']}
-            </div>
-            <div class="meta">
-                ${showing.film['runtime']}
-            </div>
-            <div class="description">
-                ${remainTickets}
-            </div>
-            <span class="ui label">
-                ${showing.showtime}
-            </span>
+        <div class="header">
+        ${showing.film['title']}
+        </div>
+        <div class="meta">
+        ${showing.film['runtime']}
+        </div>
+        <div class="description">
+        ${remainTickets}
+        </div>
+        <span class="ui label">
+        ${showing.showtime}
+        </span>
         </div>
         <div class="extra content">
-            <div class="ui blue button">Buy Ticket</div>
+        <div class="ui blue button">Buy Ticket</div>
         </div>
-    `
-    showCards.append(showCard)
-    // console.log(document.querySelector('.ui.blue.button'))
-}
-
+        `
+        showCards.append(showCard)
+        // console.log(document.querySelector('.ui.blue.button'))
+    }
+})
+    
