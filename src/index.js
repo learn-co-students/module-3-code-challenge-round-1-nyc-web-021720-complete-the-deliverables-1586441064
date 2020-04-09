0e892b596262739c75cce36f51a34d6e1c6e178f
haihuanchen <haihuan.chen@hotmail.com>
2020-04-09 10:31:16 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-const theatreId = null;
+// fetch get request to show all the movies
+//add event listener to buy tickets
+//when user buy a ticket, fetch get Post method
+// when user buy a ticket, decrease remaining tickets
+//when tickets are sold out, disable buy ticket button
+// buy ticket button.textContent should be 'sold out'
+const theatreId = 'https://evening-plateau-54365.herokuapp.com/theatres/367' ;
+
+document.addEventListener('DOMContentLoaded', () =>{
+    fetchShows(theatreId)
+
+
+
+
+
+})
+
+function fetchShows(url){
+    fetch(url)
+        .then(res => res.json())
+        .then(object => {
+            object.showings.forEach(showing => {
+                renderShow()
+            })
+        })
+}
+
