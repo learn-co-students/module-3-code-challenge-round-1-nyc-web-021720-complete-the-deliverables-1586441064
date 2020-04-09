9149a37828b7403bf8f65e6484927c1c628469ab
github username <github email address>
2020-04-09 11:36:51 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 const theatreId = 369;
 
+const showings = document.getElementsByClassName("ui cards showings")[0]
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    const showings = document.getElementsByClassName("ui cards showings")[0]
-    
+    renderShows()
 
+    document.addEventListener('click', (e) => {
+        if (e.target.textContent === 'Buy Ticket') {
+            showingId = e.target.dataset.id
+            purchaseTicket()
+            remainingTicketsNode = e.target.parentNode.parentNode.childNodes[1].childNodes[5]
+            text = remainingTicketsNode.textContent
+            ticketCount = parseInt(text)
+            ticketCount--
+            remainingTicketsNode.textContent = `${ticketCount} remaining tickets`
+            if (ticketCount <= 0) {
+                remainingTicketsNode.textContent = `${0} remaining tickets`
+                e.target.parentNode.innerHTML = ' <p>Sold Out</p>'
+            }
+        }
+    })
+})
+
+function renderShows() {
     fetch(`https://evening-plateau-54365.herokuapp.com/theatres/${theatreId}`)
     .then(response => {
         return response.json()
@@ -14,69 +33,44 @@ document.addEventListener('DOMContentLoaded', () => {
             createShowing(showing)
         });
     })
+}
 
-    document.addEventListener('click', (e) => {
-        if (e.target.textContent === 'Buy Ticket') {
-            showingId = e.target.dataset.id
-            
-            fetch('https://evening-plateau-54365.herokuapp.com/tickets', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-                body: JSON.stringify({showing_id: showingId})
-            })
-            .then(results => {
-                return results.json()
-            })
-            .then(newShowing => {
-                console.log(newShowing)
-            })
-
-            
-            remainingTicketsNode = e.target.parentNode.parentNode.childNodes[1].childNodes[5]
-            text = remainingTicketsNode.textContent
-            ticketCount = parseInt(text)
-            ticketCount--
-            remainingTicketsNode.textContent = `${ticketCount} remaining tickets`
-            if (ticketCount === 0) {
-                e.target.parentNode.innerHTML = ' <p>Sold Out</p>'
-                
-            } 
-
-        }
-    })
-
-    function createShowing(showing) {
+function createShowing(showing) {
         
-        div = document.createElement('div')
-        div.className = 'card'
-
-        numOfTickets = showing.capacity - showing.tickets_sold
-
-        div.innerHTML = `
-            <div class="card">
-                <div class="content">
-                    <div class="header">
-                        ${showing.film.title}
-                    </div>
-                    <div class="meta">
-                        ${showing.film.runtime} minutes
-                    </div>
-                    <div class="description">
-                        ${numOfTickets} remaining tickets
-                    </div>
-                    <span class="ui label">
-                        ${showing.showtime}
-                    </span>
-                    </div>
-                    <div class="extra content">
-                    <div class="ui blue button" data-id=${showing.id}>Buy Ticket</div>
+    div = document.createElement('div')
+    div.className = 'card'
+    numOfTickets = showing.capacity - showing.tickets_sold
+    div.innerHTML = `
+        <div class="card">
+            <div class="content">
+                <div class="header">
+                    ${showing.film.title}
                 </div>
+                <div class="meta">
+                    ${showing.film.runtime} minutes
+                </div>
+                <div class="description">
+                    ${numOfTickets} remaining tickets
+                </div>
+                <span class="ui label">
+                    ${showing.showtime}
+                </span>
+                </div>
+                <div class="extra content">
+                <div class="ui blue button" data-id=${showing.id}>Buy Ticket</div>
             </div>
-        `
+        </div>
+    `
+    showings.append(div)
+}
 
-        showings.append(div)
-    }
-})
+function purchaseTicket(params) {
+    fetch('https://evening-plateau-54365.herokuapp.com/tickets', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({showing_id: showingId})
+    })
+}
