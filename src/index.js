defc2262eb7d378d65b6fa6c92d7df6d5c644a59
komald4 <komald.4@gmail.com>
2020-04-09 11:01:51 -0400

    KD

--- src/index.js
+++ src/index.js
@@ -1,28 +1,56 @@
+const theatreId = 365
+const URL = "https://evening-plateau-54365.herokuapp.com/theatres/365"
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
-const theatreId = 365
-const URL = `https://evening-plateau-54365.herokuapp.com/theatres/${theatreId}`
 
 
 
 //fetch list of movies from URL
+//render list
+//add event listeners for buy button
 
-fetch(URL)
-.then(response => response.json())
-.then(movies => {
-    //console.log(movies)
-    movies.forEach(movie => {
-let div = renderMovie(movie)
-        main.append(div)
+    getData = () => {
+        fetch(URL)
+            .then(response => response.json())
+            .then(json => renderTheatre(json))
+    }
+
+    renderTheatre = json => {
+        let shows = json.showings
+        shows.forEach(show => {
+            createCard(show)
+    
     })
+    }
+
+    createCard = show => {
+        const cardHtml = `<div class="content">
+        <div class="header">
+            ${show.film.html}
+        </div>
+
+        <div class="meta">
+        ${show.film.runtime} minutes
+      </div>
+
+      <div class="description">
+        <span class="tickets-remaining">${show.capacity -
+            show.tickets_sold}</span> remaining tickets
+      </div>
+      <span class="ui label">
+        ${show.showtime}
+      </span>
+    </div>
+    <div class="extra content">
+      <div class="ui blue button">Buy Ticket</div>
+    </div>
+  </div>
+        
+        
+        
+        `
+    }
+
 })
-
-function renderMovie(movie){
-    let card = document.createElement('div')
-    card.setAttribute("class", "card")
-    card.dataset.id - movie.id
-}
-
-
-})
-
