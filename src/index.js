0c450b79b3f8ed82f8c3a810677cf44f71cb703d
zachbrown12 <zachmb22@gmail.com>
2020-04-09 11:21:35 -0400

    Close to Done

--- src/index.js
+++ src/index.js
@@ -13,11 +13,30 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(obj => renderMovies(obj))
     }
 
+    //fetch for updating tickets
+
+    function updateMovie(id) {
+        fetch(`https://evening-plateau-54365.herokuapp.com/tickets`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                showing_id: id
+            })
+        })
+        .then(r => r.json())
+        .then(resp => updateTickUI(resp))
+    }
+
+
     function renderMovies(theater) {
          let movies = theater.showings
         for (let movie of movies) {
             let newMovie = document.createElement('div')
             newMovie.className = "card"
+            newMovie.dataset.cardId = movie.id
             newMovie.innerHTML = `
              <div class="content">
                <div class="header">
@@ -34,10 +53,35 @@ document.addEventListener("DOMContentLoaded", () => {
                  </span>
                </div>
                <div class="extra content">
-               <div class="ui blue button" data-id="${movie.id}>Buy Ticket</div>
+               <div class="ui blue button" data-id="${movie.id}">Buy Ticket</div>
               </div>
             `
             movieContainer.appendChild(newMovie)
+            newMovie.addEventListener("click", handleClick)
+        }
+    }
+
+    function handleClick(event) {
+        if (event.target.className === 'ui blue button') {
+             let movieId = (event.target.dataset.id)
+             updateMovie(movieId)
+
+        }
+    }
+
+    function updateTickUI(resp){
+        if (resp.error) {
+            console.log(resp)
+            }
+        else {
+            let movies = document.querySelectorAll('.card')
+            for (let movie of movies) {
+                if (resp.showing_id === movie.dataset.contentId) {
+                    let remain = movie.querySelector('.description')
+                    console.log(remain)
+                    remain.textContent -= 1
+                }
+            }
         }
     }
 
